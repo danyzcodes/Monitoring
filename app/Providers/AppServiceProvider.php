b10b21ddaf4d9bccd40868928cb80bd7aa4c81d9
Dany <danyit1b@gmail.com>
2026-05-09 18:28:33 +0700

    Fix: Force HTTPS for Vite assets in production to resolve missing styles on login page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 use App\View\Composers\OverdueComposer;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
+        
         View::composer(['layouts.app', 'profile.edit', 'notifications.index'], OverdueComposer::class);
     }
 }
