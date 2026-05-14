c6deffb4ab95cd36d46ffb26832d198aeb626cec
Dany Zahir <danyzahir123@gmail.com>
2026-05-14 20:51:23 +0700

    fix sistem

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -434,6 +434,7 @@
                         <path style="opacity:.75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                     </svg>
                     <span id="btnText">Masuk</span>
+                    <svg id="btnArrow" style="width:16px;height:16px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                     </svg>
                 </button>
