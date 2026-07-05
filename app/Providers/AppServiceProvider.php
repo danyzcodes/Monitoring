810c81b5463523a05e7f1d94c7a8b022d79eaa04
Dany Zahir <danyzahir123@gmail.com>
2026-07-05 21:51:23 +0700

    Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\URL;
 use App\View\Composers\OverdueComposer;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
+use Symfony\Component\Mailer\Transport\Dsn;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +26,15 @@ class AppServiceProvider extends ServiceProvider
         }
         
         View::composer(['layouts.app', 'profile.edit', 'notifications.index'], OverdueComposer::class);
+
+        Mail::extend('brevo', function (array $config) {
+            return (new BrevoTransportFactory)->create(
+                new Dsn(
+                    'brevo+api',
+                    'default',
+                    $config['key'] ?? null
+                )
+            );
+        });
     }
 }
