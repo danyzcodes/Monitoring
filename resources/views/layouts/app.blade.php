3bde1e3e56d61b1ade62e354720e3d03bccdd02a
Dany Zahir <danyzahir123@gmail.com>
2026-07-17 19:07:52 +0700

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -462,9 +462,6 @@
                                         </div>
                                         <p class="text-sm font-semibold text-slate-700 flex items-center justify-center gap-1.5">
                                             Semua beres!
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-yellow-400" viewBox="0 0 24 24" fill="currentColor">
-                                                <path d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
-                                            </svg>
                                         </p>
                                         <p class="text-[11px] text-slate-400 mt-1 max-w-[200px] leading-relaxed">
                                             Tidak ada order yang melewati tanggal komitmen
